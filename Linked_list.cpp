4683fcb4bb6dc2417b4d0e92e0dc2541a5ac3f7a
Sharvi Endait <86414701+SharviE29@users.noreply.github.com>
2021-10-18 19:31:37 +0530

    Update Linked_list.cpp
    Added Reversing a linked list, concatenating and merging 2 linked lists

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -6,7 +6,7 @@ struct Node
 {
     int data;
     struct Node *next;
-}*first=NULL; //creating a global pointer
+}*first=NULL,*second=NULL,*third=NULL; //creating a global pointer
 
 void create(int A[],int n)//Creating a linked list
 {
@@ -26,7 +26,24 @@ void create(int A[],int n)//Creating a linked list
     }
     
 }
-
+void create2(int A[],int n)//creating second ll
+{
+    int i;
+    struct Node *t,*last;
+    second=(struct Node*)malloc(sizeof(struct Node));
+    second->data=A[0];
+    second->next=NULL;
+    last=second;
+    for(i=1;i<n;i++)
+    {
+       t=(struct Node *)malloc(sizeof(struct Node));
+       t->data=A[i];
+       t->next=NULL;
+       last->next=t; 
+       last=t;
+    }
+    
+}
 void display(struct Node*p)//displaying the linked list
 {
   while(p!=NULL)
@@ -231,6 +248,93 @@ void RemoveDuplicate(struct Node *p)
             q=p->next;
         }
     }
+}
+void Reverse1(struct Node *p)
+{
+    int *A,i=0;
+    struct Node *q;
+    A=(int *)malloc(sizeof(int)*count(p));
+    while(q!=NULL)
+    {
+        A[i]=q->data;
+        q=q->next;
+        i++;
+    }
+    q=p;
+    i--;
+    while(q!=NULL)
+    {
+        q->data=A[i];
+        q=q->next;
+        i--;
+    }
+}
+void Reverse2(struct Node *p)
+{
+  struct Node *q=NULL, *r=NULL;
+  while(p!=NULL)
+  {
+      r=q;
+      q=p;
+      p=p->next;
+      q->next=r;
+  }
+  first=q;
+}
+void Reverse3(struct Node *q,struct Node *p)
+{
+    if(p)
+    {
+        Reverse3(p,p->next);
+        p->next=q;
+    }
+    else 
+    {
+        first=q;
+    }
+}
+void Concat(struct Node *p, struct Node *q)
+{
+    third=p;
+    while(p->next!=NULL)
+    {
+        p=p->next;
+    }
+    p->next=q;
+}
+void Merge(struct Node *p,struct Node *q)
+{
+    struct Node *last;
+    if(p->data < q->data)
+    {
+        third=last=p;
+        p=p->next;
+        third->next=NULL;
+    }
+    else 
+    {
+        third=last=q;
+        q=q->next;
+        third->next=NULL;
+    }
+    while(p && q)
+    {
+       if(p->data < q->data)
+      {
+        last->next=p;
+        last=p;
+        p=p->next;
+        last->next=NULL;
+       }
+       else
+       {
+          last->next=q;
+          last=q;
+          q=q->next;
+          last->next=NULL;  
+       }
+       if(p)=last->next=p;
+       if(q)=last->next=q;
 }
 int main()
 {   
