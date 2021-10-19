b497d48a0806f17127b35394b5692016ae90f446
Sharvi Endait <86414701+SharviE29@users.noreply.github.com>
2021-10-19 12:42:59 +0530

    Added Sparse matrix

---
+++ sparseMatrix.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+struct Element
+{
+    int i;
+    int j;
+    int x;
+};
+struct Sparse
+{
+    int m;//stores number of rows
+    int n;//stores number of columns
+    int num;//stores the number of elements
+    struct Element *ele;//creating a dynamic array that will store the array of elements
+};
+void Create(struct Sparse *s)
+{
+    cout<<"Enter the dimensions: "<<endl;
+    scanf("%d,%d",&s->m,&s->n);
+    cout<<"Number of non zero elements:"<<endl;
+    scanf("%d",&s->num);
+    s->ele=(struct Element *)malloc(s->num*sizeof(struct Element));
+    cout<<"Enter all non zero elements:"<<endl;
+    for(int i=0;i<s->num;i++)
+    {
+        scanf("%d,%d,%d",&s->ele[i].i,&s->ele[i].j,&s->ele[i].x);
+    }
+}
+void Display(struct Sparse s)
+{
+    int i,j,k=0;
+    for(int i=0;i<s.m;i++)
+    {
+        for(j=0;j<s.n;j++)
+        {
+            if(i==s.ele[k].i && j==s.ele[k].j)
+            {
+                cout<<s.ele[k++].x;
+            }
+            else cout<<"0"<<endl;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    struct Sparse s;
+    Create(&s);
+    Display(s);
+    return 0;
+}
