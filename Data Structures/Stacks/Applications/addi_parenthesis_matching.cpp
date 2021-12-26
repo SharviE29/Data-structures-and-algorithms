ca65592993761da99820b68564a84b35eee95ee7
Sharvi Endait <sharviendait@gmail.com>
2021-12-26 21:12:24 +0530

    Create addi_parenthesis_matching.cpp

---
+++ Data Structures/Stacks/Applications/addi_parenthesis_matching.cpp
@@ -0,0 +1,117 @@
+/************************
+ 
+ Stacks using parenthesis matching
+ With using brackets of type {},[],()
+
+ ************************/
+
+#include<iostream>
+using namespace std;
+class Node
+{ 
+  public:
+  char data;
+  Node *next;
+};
+class Stack
+{  
+private:
+   int size;
+   Node *top;
+   char *s;
+public:
+   Stack()
+   {
+       top=NULL;
+   }
+   void Push(char x)
+   { 
+       Node *t=new Node();
+       if(t==NULL)
+       {
+           cout<<"Stack overflow !!!!"<<endl;
+
+       }
+       else
+       {
+            t->data=x;
+            t->next=top;
+            top=t;
+       }
+   }
+   char Pop()
+   {   
+       char x=-1;
+       Node *t=new Node();
+       if(t==NULL)
+       {
+           cout<<"Stack underflow !!!!"<<endl;
+       }
+       else
+       {
+           x=t->data;
+           t=top;
+           top=top->next;
+           delete t;
+       }
+       return x;
+   }
+   int isEmpty()
+   {
+       if(top)
+       {
+           return 1;
+       }
+       else
+       {
+           return 0;
+       }
+   }
+   int isBalanced(char *exp);
+};
+int Stack::isBalanced(char *exp)
+{
+    int i;
+    for(i=0;exp[i]!='\0';i++)
+    {
+        if(exp[i]=='(' || exp[i]=='['|| exp[i]=='{')
+        {
+            Push(exp[i]);
+        }
+        else if(exp[i]==')'|| exp[i]=='}'||exp[i]==']')
+        {
+            if(top!=NULL)
+            {
+                Pop();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    if(top==NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    char *exp="([a+b]-{c*d})";
+    Stack st;
+    int result=st.isBalanced(exp);
+    if (result==1)
+    {
+        cout<<"The expression is well balanced !!"<<endl;
+    }
+    else
+    {
+        cout<<"The expression isn't well balanced !!"<<endl;
+    }
+    return 0;
+}
