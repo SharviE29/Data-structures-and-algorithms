b15454b6b78834d3a8be8a312928d0b70148d976
Sharvi Endait <sharviendait@gmail.com>
2021-12-27 21:45:31 +0530

    Create shell_sort.py

---
+++ Algorithms/Sorting/shell_sort.py
@@ -0,0 +1,38 @@
+'''
+Shell sort 
+
+'''
+
+def shellSort(arr):
+    gap=len(arr)//2
+
+    while gap>0:
+        i=0
+        j=gap
+        while j<len(arr):
+            if arr[i]>arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+            i+=1
+            j+=1
+            k=i
+            while k-gap>-1:
+                if arr[k-gap]>arr[k]:
+                    arr[k-gap],arr[k]=arr[k],arr[k-gap]
+                k-=1
+        gap=gap//2
+
+def display():
+   print(arr)
+
+
+arr=[]
+n=int(input("Enter the number of elements in the array :"))
+
+for i in range (n):
+    value=int(input(f"Enter the element at index {i+1}: "))        
+    arr.append(value) 
+print(arr)
+print("Sorted array")
+shellSort(arr)
+display()
+
